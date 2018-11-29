2a42cd0bf513be67377572070642c2c28562f2a9
conrowland32 <conrowland32@gmail.com>
2018-11-29 02:12:30 -0600

    Finished implementation

--- Mancala.py
+++ Mancala.py
@@ -6,17 +6,12 @@ import multiprocessing
 from io import StringIO
 
 DEPTH = 5
-DONT_SCORE_ONE = True
-PLAYER_CODE = 0
-OPPONENT_CODE = 0
+DONT_SCORE_ONE = False
 
 
 def compute(x, player):
     move_sequence, board = x
-    if player:
-        return [x + 1 for x in move_sequence], board.mini_max_alpha_beta(DEPTH, maximizing_player=(not player))
-    else:
-        return [x + 1 for x in move_sequence], -board.mini_max_alpha_beta(DEPTH, maximizing_player=(not player))
+    return [x + 1 for x in move_sequence], board.mini_max_alpha_beta(DEPTH, player_turn=not player, searching_player=player)
 
 
 class Board:
@@ -42,13 +37,13 @@ class Board:
         else:
             return self.board[0]
 
-    def __init__(self, board=None):
+    def __init__(self, board=None, player_code="100000", opponent_code="100000"):
         if board is not None:
             self.board = board.board[:]
-            self.reversed = board.reversed
         else:
             self.board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4]
-            self.reversed = False
+        self.player_code = player_code
+        self.opponent_code = opponent_code
 
     def make_player_move(self, n):
         assert n < 6
@@ -63,16 +58,42 @@ class Board:
                 n = 1
             self.board[n] += 1
 
-        if n == self.PLAYER_SCORE_HOLDER:
+        if n == 7:
             return True
 
         if self.board[n] == 1 and 0 < n < 7:
             oponent_pos = len(self.board) - n
             if DONT_SCORE_ONE is False or (DONT_SCORE_ONE is True and self.board[oponent_pos] != 0):
                 self.board[n] = 0
-                self.board[self.PLAYER_SCORE_HOLDER] += 1 + \
-                    self.board[oponent_pos]
+                self.board[7] += 1 + self.board[oponent_pos]
                 self.board[oponent_pos] = 0
+
+        return False
+
+    def make_opponent_move(self, n):
+        assert n < 6
+        n += 8
+        tokens = self.board[n]
+        assert tokens > 0
+        self.board[n] = 0
+        while tokens:
+            tokens -= 1
+            n += 1
+            if n >= len(self.board):
+                n = 0
+            if n == 7:
+                n = 8
+            self.board[n] += 1
+
+        if n == 0:
+            return True
+
+        if self.board[n] == 1 and 7 < n < 14:
+            player_pos = len(self.board) - n
+            if DONT_SCORE_ONE is False or (DONT_SCORE_ONE is True and self.board[player_pos] != 0):
+                self.board[n] = 0
+                self.board[0] += 1 + self.board[player_pos]
+                self.board[player_pos] = 0
 
         return False
 
@@ -81,88 +102,84 @@ class Board:
             if a > 0:
                 yield i
 
+    def possible_opponent_moves(self):
+        for i, a in enumerate(self.board[8:]):
+            if a > 0:
+                yield i
+
     def get_player_moves(self, pos, seq, moves):
         assert self.board[1 + pos] != 0
 
-        new_board = Board(self)
+        new_board = Board(self, self.player_code, self.opponent_code)
         move_continue = new_board.make_player_move(pos)
         if move_continue and list(new_board.possible_player_moves()):
-
             for i in new_board.possible_player_moves():
                 new_board.get_player_moves(i, seq + [pos], moves)
         else:
             moves.append((seq + [pos], new_board))
             return
 
-    def find_all_moves(self):
-        all_moves = []
-        for i in self.possible_player_moves():
-            self.get_player_moves(i, [], all_moves)
+    def get_opponent_moves(self, pos, seq, moves):
+        assert self.board[8 + pos] != 0
 
+        new_board = Board(self, self.player_code, self.opponent_code)
+        move_continue = new_board.make_opponent_move(pos)
+        if move_continue and list(new_board.possible_opponent_moves()):
+            for i in new_board.possible_opponent_moves():
+                new_board.get_opponent_moves(i, seq + [pos], moves)
+        else:
+            moves.append((seq + [pos], new_board))
+            return
+
+    def find_all_moves(self, player):
+        all_moves = []
+        if player:
+            for i in self.possible_player_moves():
+                self.get_player_moves(i, [], all_moves)
+        else:
+            for i in self.possible_opponent_moves():
+                self.get_opponent_moves(i, [], all_moves)
         return all_moves
 
-    def get_opponent_board(self):
-        b = Board()
-        b.board = self.board[7:] + self.board[:7]
-        b.reversed = not self.reversed
-        return b
-
     def no_more_moves(self):
-        if any(self.board[8:]) == False or any(self.board[1:7]) == False:
+        if not any(self.board[8:]) or not any(self.board[1:7]):
             return True
         return False
 
-    def mini_max(self, depth=2, maximizing_player=False):
+    def mini_max_alpha_beta(self, depth=5, alpha=-9999, beta=+9999, player_turn=False, searching_player=True, maximizing_player=False):
         if depth == 0 or self.no_more_moves():
-            return self.get_heurestic_score()
+            return self.get_heuristic_score(searching_player)
 
         if maximizing_player:
-            best_value = -999
-            for move, board in self.get_opponent_board().find_all_moves():
-                val = board.mini_max(depth - 1, not maximizing_player)
-                best_value = max(best_value, val)
-            return best_value
-        else:
-            best_value = 999
-            for move, board in self.get_opponent_board().find_all_moves():
-                val = board.mini_max(depth - 1, not maximizing_player)
-                best_value = min(best_value, val)
-            return best_value
-
-    def mini_max_alpha_beta(self, depth=2, alpha=-999, beta=+999, maximizing_player=False):
-        if depth == 0 or self.no_more_moves():
-            return self.get_heurestic_score()
-
-        if maximizing_player:
-            best_value = -999
-            for move, board in self.get_opponent_board().find_all_moves():
+            best_value = -9999
+            for move, board in self.find_all_moves(player_turn):
                 best_value = max(best_value, board.mini_max_alpha_beta(
-                    depth - 1, alpha, beta, not maximizing_player))
+                    depth - 1, alpha, beta, not player_turn, searching_player, not maximizing_player))
                 alpha = max(alpha, best_value)
                 if beta <= alpha:
                     break
             return best_value
         else:
-            best_value = 999
-            for move, board in self.get_opponent_board().find_all_moves():
+            best_value = 9999
+            for move, board in self.find_all_moves(player_turn):
                 best_value = min(best_value, board.mini_max_alpha_beta(
-                    depth - 1, alpha, beta, not maximizing_player))
+                    depth - 1, alpha, beta, not player_turn, searching_player, not maximizing_player))
                 beta = min(beta, best_value)
                 if beta <= alpha:
                     break
             return best_value
 
     def find_best_move(self, n=1, player=True):
-        if player and PLAYER_CODE == "000000":
+        if player and self.player_code == "000000":
             return self.find_random_move(n, player)
-        if not player and OPPONENT_CODE == "000000":
+        if not player and self.opponent_code == "000000":
             return self.find_random_move(n, player)
 
         print("Calculating best move...")
         t = time()
 
         params = []
-        for move in list(self.find_all_moves()):
+        for move in list(self.find_all_moves(player)):
             params.append((move, player))
 
         def moves():
@@ -177,7 +194,7 @@ class Board:
         print("Calculating random move...")
         t = time()
 
-        random_move = rand.choice(list(self.find_all_moves()))[0]
+        random_move = rand.choice(list(self.find_all_moves(player)))[0]
         for i in range(len(random_move)):
             random_move[i] = random_move[i] + 1
         result = [(random_move, 0)]
@@ -203,14 +220,71 @@ class Board:
         print(*["%2d" % x for x in self.board[1:7]], sep="|", file=result)
         return result.getvalue()
 
-    def get_heurestic_score(self):
-        # Player turn
-        if not self.reversed:
+    def get_heuristic_score(self, player):
+        total_count = 0
+        if player:
+            if self.player_code[0] == '1':
+                total_count += self.get_h1(player)
+            if self.player_code[1] == '1':
+                total_count += self.get_h2(player)
+            if self.player_code[2] == '1':
+                total_count += self.get_h3(player)
+            if self.player_code[3] == '1':
+                total_count += self.get_h4(player)
+            if self.player_code[4] == '1':
+                total_count += self.get_h5(player)
+            if self.player_code[5] == '1':
+                total_count += self.get_h6(player)
+        else:
+            if self.opponent_code[0] == '1':
+                total_count += self.get_h1(player)
+            if self.opponent_code[1] == '1':
+                total_count += self.get_h2(player)
+            if self.opponent_code[2] == '1':
+                total_count += self.get_h3(player)
+            if self.opponent_code[3] == '1':
+                total_count += self.get_h4(player)
+            if self.opponent_code[4] == '1':
+                total_count += self.get_h5(player)
+            if self.opponent_code[5] == '1':
+                total_count += self.get_h6(player)
+        return total_count
+
+    def get_h1(self, player):
+        if player:
             return self.player_points - self.opponent_points
-        # Opponent turn
         else:
             return self.opponent_points - self.player_points
 
+    def get_h2(self, player):
+        if player:
+            return self.player_points - sum(self.board)/2
+        else:
+            return self.opponent_points - sum(self.board)/2
+
+    def get_h3(self, player):
+        if player:
+            return sum(self.board)/2 - self.opponent_points
+        else:
+            return sum(self.board)/2 - self.player_points
+
+    def get_h4(self, player):
+        if player:
+            return sum(self.board[5:7])
+        else:
+            return sum(self.board[12:])
+
+    def get_h5(self, player):
+        if player:
+            return sum(self.board[1:3])
+        else:
+            return sum(self.board[8:10])
+
+    def get_h6(self, player):
+        if player:
+            return sum(self.board[3:5])
+        else:
+            return sum(self.board[10:12])
 
 
 def player_move(board, moves=None):
@@ -240,13 +314,11 @@ def player_move(board, moves=None):
 
 
 def opponent_move(board, moves=None):
-    board = board.get_opponent_board()
-
     if moves:
         for move in moves[0]:
-            board.make_player_move(move - 1)
-            board.get_opponent_board().print()
-        return board.get_opponent_board()
+            board.make_opponent_move(move - 1)
+            board.print()
+        return board
 
     has_move = True
     while has_move:
@@ -257,32 +329,33 @@ def opponent_move(board, moves=None):
             sys.exit(0)
         try:
             c = int(command[0])
-            has_move = board.make_player_move(c - 1)
-            board.get_opponent_board().print()
+            has_move = board.make_opponent_move(c - 1)
+            board.print()
         except:
             print('Wrong move: ', command[0])
             continue
 
-    return board.get_opponent_board()
+    return board
 
 
-def run_game(initial_board=None, player_starts=True):
-    board = Board()
-    if initial_board is not None:
-        board.board = initial_board
+def run_game(player_starts=True, player_code="100000", opponent_code="100000"):
+    board = Board(None, player_code, opponent_code)
 
     board.print()
 
-    while 1:
+    while True:
         if board.no_more_moves():
             player_side = board.board[1:8]
             opponent_side = board.board[8:] + board.board[:1]
             if sum(player_side) < sum(opponent_side):
-                print("Game over  -  Opponent wins", sum(opponent_side), "-", sum(player_side))
+                print("Game over  -  Opponent wins",
+                      sum(opponent_side), "-", sum(player_side))
             elif sum(player_side) > sum(opponent_side):
-                print("Game over  -  Player wins", sum(player_side), "-", sum(opponent_side))
+                print("Game over  -  Player wins",
+                      sum(player_side), "-", sum(opponent_side))
             else:
-                print("Game over  -  Draw", sum(player_side), "-", sum(opponent_side))
+                print("Game over  -  Draw", sum(player_side),
+                      "-", sum(opponent_side))
             break
 
         if player_starts:
@@ -291,11 +364,11 @@ def run_game(initial_board=None, player_starts=True):
             board = player_move(board, best_move)
             if board.no_more_moves():
                 continue
-            for best_move in board.get_opponent_board().find_best_move(5, False):
+            for best_move in board.find_best_move(5, False):
                 print(best_move)
             board = opponent_move(board, best_move)
         else:
-            for best_move in board.get_opponent_board().find_best_move(5, False):
+            for best_move in board.find_best_move(5, False):
                 print(best_move)
             board = opponent_move(board, best_move)
             if board.no_more_moves():
@@ -307,17 +380,10 @@ def run_game(initial_board=None, player_starts=True):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Mancala AI')
-    parser.add_argument('-b', '--board', default=None)
-    parser.add_argument('-d', '--depth', type=int, default=5)
     parser.add_argument('-o', '--opponent-starts',
                         default=False, action="store_true")
-    parser.add_argument('--dont-score-one', default=False, action="store_true")
     parser.add_argument('-p', '--player-code', default="100000")
     parser.add_argument('-e', '--opponent-code', default="100000")
     args = parser.parse_args()
 
-    DEPTH = args.depth
-    DONT_SCORE_ONE = args.dont_score_one
-    PLAYER_CODE = args.player_code
-    OPPONENT_CODE = args.opponent_code
-    run_game(args.board, not args.opponent_starts)
+    run_game(not args.opponent_starts, args.player_code, args.opponent_code)
